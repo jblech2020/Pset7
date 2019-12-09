0607cba4b6562cd7d0a8dc02a9e52f23fe7d08d8
jblech2020 <54987804+jblech2020@users.noreply.github.com>
2019-12-09 14:42:50 -0500

    Teacher.java is done for now
    PlEaSe MaKe LeSs COMmIts PleAsE

--- Teacher.java
+++ Teacher.java
@@ -1,6 +1,7 @@
 package com.apcsa.model;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.apcsa.model.User;
 
@@ -11,23 +12,32 @@ public class Teacher extends User {
     private String firstName;
     private String lastName;
 
-    public Teacher(int teacherId, int departmentId, String firstName, String lastName) {
-        this.teacherId = teacherId;
-        this.departmentId = departmentId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-	public Teacher(User user, ResultSet rs) {
-		// TODO Auto-generated constructor stub
+	public Teacher(User user, ResultSet rs) throws SQLException {
+		super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user.getLastLogin());
+		this.teacherId = rs.getInt("teacher_id");
+		this.departmentId = rs.getInt("department_id");
+		this.firstName = rs.getString("first_name");
+		this.lastName = rs.getString("last_name");
 	}
 
-//    public Teacher(int teacherId, int departmentId, String firstName, String lastName) {
-//        this.teacherId = teacherId;
-//        this.departmentId = departmentId;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//    }
+	/*
+	 * Getters
+	 */
 
+	public int getTeacherId() {
+		return teacherId;
+	}
 
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	
 }
