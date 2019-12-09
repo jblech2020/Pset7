fc11322043560e67ff943d31621d637b29910ef8
drogers12 <54987779+drogers12@users.noreply.github.com>
2019-12-09 08:57:35 -0500

    Add files via upload
    good commit

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -4,12 +4,24 @@ import com.apcsa.model.User;
 
 public class Student extends User {
 
-    private int studentId;
+  
+	public Student(int userId, String accountType, String username, String password, String lastLogin) {
+		super(userId, accountType, username, password, lastLogin);
+	}
+
+	private int studentId;
     private int classRank;
     private int gradeLevel;
     private int graduationYear;
     private double gpa;
     private String firstName;
     private String lastName;
-    
+	private String user;
+	private String rs;
+
+    public void student(String user, String rs){
+      this.user = user;
+      this.rs = rs;
+    }
+
 }
