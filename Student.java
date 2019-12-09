4f1321d7ed4466e174e723ef2fe7ed562190be8d
jblech2020 <54987804+jblech2020@users.noreply.github.com>
2019-12-09 14:32:42 -0500

    Finished Student.java (for now)
    aight

--- Student.java
+++ Student.java
@@ -1,6 +1,7 @@
 package com.apcsa.model;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.apcsa.model.User;
 
@@ -14,11 +15,64 @@ public class Student extends User {
     private String firstName;
     private String lastName;
 	
-	public Student(User user, ResultSet rs) {
-		super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user..getLastLogin());
-		this.studentId = rs.getInt();
-		this.classRank = rs.getInt();
-		
-				
+	public Student(User user, ResultSet rs) throws SQLException {
+		super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user.getLastLogin());
+		this.studentId = rs.getInt("student_id");
+		this.setClassRank(rs.getInt("class_rank"));
+		this.setGradeLevel(rs.getInt("grade_level"));
+		this.graduationYear = rs.getInt("graduation");
+		this.setGpa(rs.getDouble("gpa"));
+		this.firstName = rs.getString("first_name");
+		this.lastName = rs.getString("last_name");
 	}
+
+	/*
+	 * Getters
+	 */
+	
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public int getClassRank() {
+		return classRank;
+	}
+
+	public int getGradeLevel() {
+		return gradeLevel;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+	
+	public int getGraduationYear() {
+		return graduationYear;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/*
+	 * Setters
+	 */
+
+	public void setGradeLevel(int gradeLevel) {
+		this.gradeLevel = gradeLevel;
+	}
+	
+	public void setClassRank(int classRank) {
+		this.classRank = classRank;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
 }
+
