22ace9d64f5367797acf8bc1764e171c0e52c6bd
jblech2020 <54987804+jblech2020@users.noreply.github.com>
2019-12-11 08:10:32 -0500

    Admin done...
    yes

--- Administrator.java
+++ Administrator.java
@@ -1,23 +1,50 @@
 package com.apcsa.model;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.apcsa.model.User;
 
 public class Administrator extends User {
-
-    public Administrator(int userId, String accountType, String username, String password, String lastLogin) {
-		super(userId, accountType, username, password, lastLogin);
-		// TODO Auto-generated constructor stub
-	}
-    
-	public Administrator(User user, ResultSet rs) {
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	private int administratorId;
     private String firstName;
     private String lastName;
     private String jobTitle;
+    
+    public Administrator(User user, ResultSet rs) throws SQLException {
+		super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user.getLastLogin());
+		this.setJobTitle(rs.getString("job_title"));
+		this.lastName = rs.getString("last_name");
+		this.firstName = rs.getString("first_name");
+		this.administratorId = rs.getInt("administrator_id");
+    }
 
+	/*
+	 * Getters
+	 */
+    
+	public int getAdministratorId() {
+		return administratorId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	
+    /*
+	 * Setters
+	 */
+	
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
 }
